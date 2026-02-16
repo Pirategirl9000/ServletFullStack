8dc00f15d9fe86efa52e6ac03ba1e922c0663760
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-02-16 09:36:26 -0600

    Added javadocs

--- src/main/java/org/example/servletfullstack/controllers/ProductController.java
+++ src/main/java/org/example/servletfullstack/controllers/ProductController.java
@@ -8,24 +8,47 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+/**
+ * Handles HTTP requests sent to the server and delegates the work to the service layer
+ */
 @WebServlet(name = "productController", value = "/product-controller")
 public class ProductController extends HttpServlet {
 
+    /**
+     * Code that runs when the Servlet is initialilized
+     */
     public void init() {
 
     }
 
+    /**
+     * Handles the GET requests
+     * @param request the request sent
+     * @param response the response to send
+     * @throws ServletException if an error with the servlet occurs
+     * @throws IOException if an error occurs during IO
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 
     }
 
+    /**
+     * Handles the POST requests
+     * @param request the request sent
+     * @param response the response to send
+     * @throws ServletException if an error with the servlet occurs
+     * @throws IOException if an error occurs during IO
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 
 
     }
 
+    /**
+     * Code that runs when Servlet is being terminated
+     */
     public void destroy() {
 
 
