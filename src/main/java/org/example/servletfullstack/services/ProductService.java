8546d316e64e14b12feb08c33836d38bdea45692
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-02-16 09:41:04 -0600

    Created basic structure for the ProductService class

--- src/main/java/org/example/servletfullstack/services/ProductService.java
+++ src/main/java/org/example/servletfullstack/services/ProductService.java
@@ -1,4 +1,28 @@
 package org.example.servletfullstack.services;
 
+/**
+ * Handles the operations passed to it by the Controller layer. Is a singleton class to prevent database access issues
+ */
 public class ProductService {
+    /**
+     * The singleton instance of the ProductService
+     */
+    private ProductService productService;
+
+    /**
+     * A private constructor for ProductService to acheive singleton status
+     */
+    private ProductService() {}
+
+    /**
+     * Returns the reference to a ProductService object
+     * @return ProductService object
+     */
+    public ProductService getProductService() {
+        if (productService == null) {
+            productService = new ProductService();
+        }
+
+        return productService;
+    }
 }
