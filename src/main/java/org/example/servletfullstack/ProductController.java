0b44f7091df88c8b906d997a4ae39885a1a15d47
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-02-16 09:31:11 -0600

    Created a class for handling product requests using CSR web model. Created skeleton for methods that will be used

---
+++ src/main/java/org/example/servletfullstack/ProductController.java
@@ -0,0 +1,35 @@
+package org.example.servletfullstack;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "productController", value = "/product-controller")
+public class ProductController extends HttpServlet {
+
+    public void init() {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+    }
+
+    public void destroy() {
+
+
+    }
+
+
+}
