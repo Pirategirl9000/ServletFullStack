05bbe08f805a7ebccc4909d96fad877174d351c8
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-02-16 16:22:49 -0600

    Created skeleton of methods for the product repository

--- src/main/java/org/example/servletfullstack/repositories/ProductRepo.java
+++ src/main/java/org/example/servletfullstack/repositories/ProductRepo.java
@@ -1,4 +1,31 @@
 package org.example.servletfullstack.repositories;
 
+
 public class ProductRepo {
+    /**
+     * The singleton instance of the ProductRepo
+     */
+    private static ProductRepo productRepo;
+
+    /**
+     * A private constructor for ProductRepo to acheive singleton status
+     */
+    private ProductRepo() {}
+
+    /**
+     * Returns the reference to a ProductRepo object
+     * @return productRepo object
+     */
+    synchronized public static ProductRepo getProductRepo() {
+        if (productRepo == null) productRepo = new ProductRepo();
+
+        return productRepo;
+    }
+
+    /**
+     * Shuts down the service class
+     */
+    public void destroy() {
+
+    }
 }
