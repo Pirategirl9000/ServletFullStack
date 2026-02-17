67900196d4e54de35e75d7e6f3f6f11f98a3e713
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-02-16 18:49:23 -0600

    Created a basic data carrier class for Products from the database

--- src/main/java/org/example/servletfullstack/repositories/objects/Product.java
+++ src/main/java/org/example/servletfullstack/repositories/objects/Product.java
@@ -1,4 +1,31 @@
 package org.example.servletfullstack.repositories.objects;
 
 public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+
+    public Product(int id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
