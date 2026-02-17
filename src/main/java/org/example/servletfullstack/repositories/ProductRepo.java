c3f659b5eb91bf7396a248bdde22b98c02398a1c
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-02-16 18:47:12 -0600

    Added method for querying all the products and converting them to Product objects to be passed back to the service layer

--- src/main/java/org/example/servletfullstack/repositories/ProductRepo.java
+++ src/main/java/org/example/servletfullstack/repositories/ProductRepo.java
@@ -3,6 +3,11 @@ package org.example.servletfullstack.repositories;
 // contains sensitive database configuration details
 // This is why it is not included in the GitHub repository
 import org.example.resources.DatabaseConfig;
+import org.example.servletfullstack.repositories.objects.Product;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductRepo {
     /**
@@ -11,10 +16,19 @@ public class ProductRepo {
     private static ProductRepo productRepo;
 
     /**
-     * A private constructor for ProductRepo to acheive singleton status
+     * The connection to the database
+     */
+    private Connection conn;
+
+    /**
+     * A private constructor for ProductRepo to acheive singleton status, initializes the database connection
      */
     private ProductRepo() {
-
+        try {
+            this.conn = DriverManager.getConnection(DatabaseConfig.getURL(), DatabaseConfig.getUser(), DatabaseConfig.getPassword());
+        } catch (SQLException e) {
+            throw new RuntimeException("Error establishing connection to database: " + e.getMessage());
+        }
     }
 
     /**
@@ -27,10 +41,39 @@ public class ProductRepo {
         return productRepo;
     }
 
+    public List<Product> getProducts() {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from products");
+
+            List<Product> products = new ArrayList<>();
+
+            while (rs.next()) {
+                products.add(
+                        new Product(
+                                rs.getInt("product_id"),
+                                rs.getString("product_name"),
+                                rs.getString("product_desc"),
+                                rs.getDouble("product_price")
+                        )
+                );
+            }
+
+            return products;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error executing query: " + e.getMessage());
+        }
+    }
+
     /**
      * Shuts down the JDBC connection
      */
     public void destroy() {
-
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error closing connection: " + e.getMessage());
+        }
     }
 }
