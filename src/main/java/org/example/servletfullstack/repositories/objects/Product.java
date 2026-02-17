f9f0329ca8aab1bf303670b93122382a33f12747
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-02-17 12:02:45 -0600

    Products now contains a String list of its categories

--- src/main/java/org/example/servletfullstack/repositories/objects/Product.java
+++ src/main/java/org/example/servletfullstack/repositories/objects/Product.java
@@ -6,5 +6,6 @@ package org.example.servletfullstack.repositories.objects;
  * @param name the name of the product
  * @param description the description for the product
  * @param price the price of the product
+ * @param categories the categories this product falls under
  */
-public record Product(int id, String name, String description, double price){}
+public record Product(int id, String name, String description, double price, String[] categories){}
