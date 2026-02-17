86fb90c9a04e1a81e2e0566f280f67039ceeb271
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-02-16 19:15:59 -0600

    Added Javadocs

--- src/main/java/org/example/servletfullstack/repositories/objects/Product.java
+++ src/main/java/org/example/servletfullstack/repositories/objects/Product.java
@@ -1,3 +1,10 @@
 package org.example.servletfullstack.repositories.objects;
 
+/**
+ * Data carrier class for product items retrieved from the database
+ * @param id the primary key id of this product
+ * @param name the name of the product
+ * @param description the description for the product
+ * @param price the price of the product
+ */
 public record Product(int id, String name, String description, double price){}
