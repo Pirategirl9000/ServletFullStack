37e187afaa4cd962a7d93594497c2f56ef254e1d
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-02-16 20:54:03 -0600

    Fixed indentation problems that arose when I messed up the code earlier

--- src/main/java/org/example/servletfullstack/repositories/ProductRepo.java
+++ src/main/java/org/example/servletfullstack/repositories/ProductRepo.java
@@ -46,71 +46,71 @@ public class ProductRepo {
         }
     }
 
-private Connection getConnection() {
-    try {
-        return DriverManager.getConnection(DatabaseConfig.getURL(), DatabaseConfig.getUser(), DatabaseConfig.getPassword());
-    } catch (SQLException e) {
-        throw new RuntimeException("Error establishing connection to database: " + e.getMessage());
-    }
-}
-
-/**
- * Returns the reference to a ProductRepo object
- * @return ProductRepo object
- */
-synchronized public static ProductRepo getProductRepo() {
-    if (productRepo == null) productRepo = new ProductRepo();
-
-    return productRepo;
-}
-
-/**
- * Queries the database for all products and returns a list of Product objects
- * @return list of product objects
- */
-synchronized public List<Product> getAllProducts() {
-    // If we have already fetched the products from the database we just return the memoized list
-    // We will pull from the database again based on how much time has passed just in case the database updates
-    // during uptime
-    if (this.products == null || System.nanoTime() - this.lastUpdate >= DURATIONTILLUPDATE) {
-        this.products = fetchProducts();
+    private Connection getConnection() {
+        try {
+            return DriverManager.getConnection(DatabaseConfig.getURL(), DatabaseConfig.getUser(), DatabaseConfig.getPassword());
+        } catch (SQLException e) {
+            throw new RuntimeException("Error establishing connection to database: " + e.getMessage());
+        }
     }
 
-    return this.products;
-}
+    /**
+     * Returns the reference to a ProductRepo object
+     * @return ProductRepo object
+     */
+    synchronized public static ProductRepo getProductRepo() {
+        if (productRepo == null) productRepo = new ProductRepo();
 
-/**
- * Fetches all products from the database and assigns them to the products attribute
- */
-synchronized private List<Product> fetchProducts() {
-    List<Product> newList = new ArrayList<>();
-    Connection conn = getConnection();
+        return productRepo;
+    }
 
-    try {
-        // Prepare and execute a statement to query the database for all products
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("select * from products");
-
-        // Go through every row of data and create a product from it to add to the array
-        while (rs.next()) {
-            newList.add(
-                    new Product(
-                            rs.getInt("product_id"),
-                            rs.getString("product_name"),
-                            rs.getString("product_desc"),
-                            rs.getDouble("product_price")
-                    )
-            );
+    /**
+     * Queries the database for all products and returns a list of Product objects
+     * @return list of product objects
+     */
+    synchronized public List<Product> getAllProducts() {
+        // If we have already fetched the products from the database we just return the memoized list
+        // We will pull from the database again based on how much time has passed just in case the database updates
+        // during uptime
+        if (this.products == null || System.nanoTime() - this.lastUpdate >= DURATIONTILLUPDATE) {
+            this.products = fetchProducts();
         }
 
-        rs.close();
-        stmt.close();
-    } catch (SQLException e) {
-        throw new RuntimeException("Error executing query: " + e.getMessage());
+        return this.products;
     }
 
-    // Track the last time we updated our memoized data
-    this.lastUpdate = System.nanoTime();
-    return newList;
-}
+    /**
+     * Fetches all products from the database and assigns them to the products attribute
+     */
+    synchronized private List<Product> fetchProducts() {
+        List<Product> newList = new ArrayList<>();
+        Connection conn = getConnection();
+
+        try {
+            // Prepare and execute a statement to query the database for all products
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from products");
+
+            // Go through every row of data and create a product from it to add to the array
+            while (rs.next()) {
+                newList.add(
+                        new Product(
+                                rs.getInt("product_id"),
+                                rs.getString("product_name"),
+                                rs.getString("product_desc"),
+                                rs.getDouble("product_price")
+                        )
+                );
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error executing query: " + e.getMessage());
+        }
+
+        // Track the last time we updated our memoized data
+        this.lastUpdate = System.nanoTime();
+        return newList;
+    }
 }
