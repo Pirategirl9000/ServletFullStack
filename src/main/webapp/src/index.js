851347c97e677fdc63587eeea42c9b4432e72207
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-03-21 21:40:05 -0500

    moved the nav bar listener application to a dedicated function

--- src/main/webapp/src/index.js
+++ src/main/webapp/src/index.js
@@ -1,17 +1,19 @@
 const navDivs = document.querySelectorAll("nav div");
 
-
-document.addEventListener("DOMContentLoaded", ()=> {
+/**
+ * For every nav bar element we add it's corresponding redirect link
+ */
+function initializeNavBar() {
     for (const div of navDivs) {
-        // If it has the data link we redirect to the page
         if (!div.hasAttribute("data-link")) return;
 
         div.addEventListener("click", ()=>{
             location.href = div.getAttribute("data-link");
         })
     }
+}
 
 
-
-
+document.addEventListener("DOMContentLoaded", ()=> {
+    initializeNavBar();
 })
