6f639384ce5d1350a330c110e390a8ed82263484
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-04-05 22:49:28 -0500

    Migrated from JSP to Thymeleaf

--- src/main/java/org/example/servletfullstack/views/HomeView.java
+++ src/main/java/org/example/servletfullstack/views/HomeView.java
@@ -8,9 +8,27 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name="HomeController", urlPatterns = {"/home", "/index"})
+@WebServlet(name="HomeView", urlPatterns = {"/home", "/index"})
 public class HomeView extends HttpServlet {
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        this.getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").forward(req, res);
+    /**
+     * The cached webpage
+     */
+    private String page;
+
+    /**
+     * Returns the /home webpage
+     * @param request an {@link HttpServletRequest} object that contains the request the client has made of the servlet
+     * @param response an {@link HttpServletResponse} object that contains the response the servlet sends to the client
+     * @throws ServletException if servlet exception occured
+     * @throws IOException if input or output exception occured
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        if (this.page != null) {
+            response.getWriter().println(this.page);
+        } else {
+            this.page = ThymeleafFacade.requestPage("home", request, response);
+            response.getWriter().println(this.page);
+        }
     }
 }
