3bf228ba7a8133c49b6e6e7021c9a04b3d717c71
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-04-05 22:45:34 -0500

    Added documentation

--- src/main/java/org/example/servletfullstack/listeners/ThymeLeafFactory.java
+++ src/main/java/org/example/servletfullstack/listeners/ThymeLeafFactory.java
@@ -6,8 +6,15 @@ import jakarta.servlet.annotation.WebListener;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
+/**
+ * ServletContextListener that builds the thymleaf engine and adds it to the servlet context on initialization
+ */
 @WebListener
 public class ThymeLeafFactory implements ServletContextListener {
+    /**
+     * Triggered when the servlet context is initialized building the thymeleaf engine and adding it to the attributes
+     * @param sce the ServletContextEvent containing the ServletContext that is being initialized
+     */
     public void contextInitialized(ServletContextEvent sce) {
 
         // Set up the template resolver for the thymleaf engine
